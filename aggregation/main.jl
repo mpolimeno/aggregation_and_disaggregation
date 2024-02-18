327f94683effc921d6dc75ea72aebb07c5aa38cc
mpolimeno <polimenomatt@gmail.com>
2024-02-18 09:53:28 -0800

    fixed typo in comment

--- aggregation/main.jl
+++ aggregation/main.jl
@@ -21,7 +21,7 @@ end
 cubes::Matrix{Integer} = zeros(number_of_cubes,dimensionality)
 
 # implement Individually-added aggregation routine
-# Refeference: https://journals.aps.org/prfluids/abstract/10.1103/PhysRevFluids.5.044305
+# Reference: https://journals.aps.org/prfluids/abstract/10.1103/PhysRevFluids.5.044305
 # constants for the routine
 deltaR::Float64 = 10 # radius of sphere on whose surface each random walker is generated
 steplength::Integer = 2
